2d182297620f3bb3fd77fac8e5e6c4d5a451798a
Shoyama Kaito <C0B2308385@edu.teu.ac.jp>
2024-11-19 15:40:53 +0900

    練習2　こうかとん

--- pygame_image.py
+++ pygame_image.py
@@ -10,12 +10,15 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    ko_img = pg.image.load("3.png")
+    ko_img = pg.transform.flip(ko_img,True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        
         pg.display.update()
         tmr += 1        
         clock.tick(10)
