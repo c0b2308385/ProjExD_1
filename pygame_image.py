10c3aa1cb2417a7d8ac2d7978b0335f195c1964f
Shoyama Kaito <C0B2308385@edu.teu.ac.jp>
2024-11-19 16:32:22 +0900

    練習8　こうかとん動かす

--- pygame_image.py
+++ pygame_image.py
@@ -15,17 +15,29 @@ def main():
     ko_img = pg.transform.flip(ko_img,True,False)
     tmr = 0
     back_x = 0
+    ko_rct = ko_img.get_rect()
+    ko_rct.center = 300, 200
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         back_x = -(tmr%3200)
 
-
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            ko_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            ko_rct.move_ip((0,+1))
+        if key_lst[pg.K_LEFT]:
+            ko_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            ko_rct.move_ip((+1,0))
+        
         screen.blit(bg_img, [back_x,0])
         screen.blit(bg_img2, [back_x+1600,0])
         screen.blit(bg_img, [back_x+3200,0])
         screen.blit(bg_img2, [back_x+4800,0])
-        screen.blit(ko_img, [300,200])
+        #screen.blit(ko_img, [300,200])
+        screen.blit(ko_img,ko_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
